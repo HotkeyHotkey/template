4623397545f8d795852fe305c6d0f13fe31c5852
HotkeyHotkey <31609972+HotkeyHotkey@users.noreply.github.com>
2017-12-10 21:50:37 +0500

    Add files via upload

---
+++ block/scripts.php
@@ -0,0 +1,3 @@
+<script src="../js/jq.js"></script>
+<script src="../js/bootstrap.min.js"></script>
+<script src="../js/unimail.js"></script>
---
+++ index.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <?php $title = 'Buy'; require_once 'block/head.php'; ?>
+  </head>
+  <body>
+
+    <?php require_once 'block/scripts.php'; ?>
+  </body>
+</html>
